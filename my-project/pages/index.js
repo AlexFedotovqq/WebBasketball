b05d7c6f978496083a256abd89b28be3e25bf5a4
Alex Fedoto <alexkekfedotov@gmail.com>
2022-11-13 16:53:42 +0100

    commit

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
             <h2 className="text-6xl font-bold underline text-center">Web3Basketball</h2>
             <p className="max-w-xl mx-auto mt-6 text-2xl leading-relaxed text-black">Web3Basketball is a platform where we talk about the rules of the game, brands, equipment, playing techniques and much more.</p>
             <p className="max-w-xl mx-auto mt-6 text-2xl leading-relaxed text-black">Our content is suitable and will be interesting not only for beginners, but also for professional players.</p>
+
         </div>
         </div>
         </section>
