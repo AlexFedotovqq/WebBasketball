d8d4972cac5c9c36b1a3d1deb75a6d29449e0a36
Alex Fedoto <alexkekfedotov@gmail.com>
2022-11-17 17:56:39 +0100

    make it better

--- my-project/pages/contact.jsx
+++ my-project/pages/contact.jsx
@@ -104,7 +104,7 @@ export default function Contact() {
                     <div class="sm:col-span-2">
                       <button
                         type="submit"
-                        class="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
+                        class="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-800 focus:bg-blue-700"
                       >
                         Send
                       </button>
--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -1,21 +1,46 @@
 export default function Home() {
   return (
-    <section className="h-screen py-10 bg-gray-200 from-fuchsia-600 to-blue-600 sm:py-16 lg:py-24">
-      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="max-w-2xl mx-auto text-center">
-          <h2 className="text-6xl font-bold underline text-center">
-            Web3Basketball
-          </h2>
-          <p className="max-w-xl mx-auto mt-6 text-2xl leading-relaxed text-black">
-            Web3Basketball is a platform where we talk about the rules of the
-            game, brands, equipment, playing techniques and much more.
-          </p>
-          <p className="max-w-xl mx-auto mt-6 text-2xl leading-relaxed text-black">
-            Our content is suitable and will be interesting not only for
-            beginners, but also for professional players.
-          </p>
+    <main className="lg:relative bg-gray-200">
+        <div className="mx-auto w-full max-w-7xl pt-16 pb-20 text-center lg:py-48 lg:text-left">
+          <div className="px-4 sm:px-8 lg:w-1/2 xl:pr-16">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+              <span className="block  xl:inline">Web3Basketball</span>{' '}
+            </h1>
+            <p className="mx-auto mt-3 max-w-md text-lg text-black sm:text-xl md:mt-5 md:max-w-3xl">
+            Web3Basketball is a platform where we talk about the rules of the game, brands, equipment, playing techniques and much more.
+            </p>
+
+            <p className="mx-auto mt-3 max-w-md text-lg text-black sm:text-xl md:mt-5 md:max-w-3xl">
+            Our content is suitable and will be interesting not only for beginners, but also for professional players.
+            </p>
+
+            <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                <a
+                  href="/Beginners"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                >
+                  For beginners
+                </a>
+              </div>
+              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                <a
+                  href="/market"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg"
+                >
+                  Market
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+        <div className="relative h-64 w-full sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:h-full lg:w-1/2">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.pexels.com/photos/1152853/pexels-photo-1152853.jpeg?auto=compress&cs=tinysrgb&w=900"
+            alt=""
+          />
+        </div>
+      </main>
   );
 }
