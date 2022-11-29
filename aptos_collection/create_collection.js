d4cdde71931053ef3fdee85fa02ed72861a7a7cc
pfed-prog <fedorovn19@gmail.com>
2022-11-29 14:08:11 +0100

    generated proper collection

--- aptos_collection/create_collection.js
+++ aptos_collection/create_collection.js
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import {
   AptosClient,
   AptosAccount,
   FaucetClient,
   TokenClient,
   CoinClient,
+  HexString,
 } from "aptos";
 
 const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
@@ -18,22 +22,19 @@ const tokenClient = new TokenClient(client); // <:!:section_1b
 const coinClient = new CoinClient(client);
 
 // Create accounts.
-// :!:>section_2
-const alice = new AptosAccount();
-const bob = new AptosAccount(); // <:!:section_2
+const privateKeyBytes = HexString.ensure(process.env.PK).toUint8Array();
+const alice = new AptosAccount(privateKeyBytes);
+// const bob = new AptosAccount();
 
 // Print out account addresses.
 console.log("=== Addresses ===");
 console.log(`Alice: ${alice.address()}`);
-console.log(`Bob: ${bob.address()}`);
 console.log("");
 
 await faucetClient.fundAccount(alice.address(), 100_000_000);
-await faucetClient.fundAccount(bob.address(), 100_000_000); // <:!:section_3
 
 console.log("=== Initial Coin Balances ===");
 console.log(`Alice: ${await coinClient.checkBalance(alice)}`);
-console.log(`Bob: ${await coinClient.checkBalance(bob)}`);
 console.log("");
 
 console.log("=== Creating Collection and Token ===");
@@ -47,15 +48,6 @@ const description =
 const URI =
   "https://bafybeie2dsp3nlrnf4pnjbrjnqqujfxazxykegr6bhvit3jurbc5kwzpki.ipfs.nftstorage.link/";
 
-const tokenId = {
-  token_data_id: {
-    creator: alice.address().hex(),
-    collection: collectionName,
-    name: tokenName,
-  },
-  property_version: `${tokenPropertyVersion}`,
-};
-
 const txnHash1 = await tokenClient.createCollection(
   alice,
   collectionName,
@@ -68,9 +60,9 @@ const txnHash2 = await tokenClient.createToken(
   alice,
   collectionName,
   tokenName,
-  "simple token", //
+  "simple token",
   1000,
-  "https://bafybeie2dsp3nlrnf4pnjbrjnqqujfxazxykegr6bhvit3jurbc5kwzpki.ipfs.nftstorage.link/0.json" //
+  "https://bafybeie2dsp3nlrnf4pnjbrjnqqujfxazxykegr6bhvit3jurbc5kwzpki.ipfs.nftstorage.link/0.json"
 );
 
 await client.waitForTransaction(txnHash2, { checkSuccess: true });
@@ -79,9 +71,9 @@ const txnHash3 = await tokenClient.createToken(
   alice,
   collectionName,
   "Shorts",
-  "simple token", //
+  "simple token",
   1000,
-  "https://bafybeie2dsp3nlrnf4pnjbrjnqqujfxazxykegr6bhvit3jurbc5kwzpki.ipfs.nftstorage.link/1.json" //
+  "https://bafybeie2dsp3nlrnf4pnjbrjnqqujfxazxykegr6bhvit3jurbc5kwzpki.ipfs.nftstorage.link/1.json"
 );
 
 await client.waitForTransaction(txnHash3, { checkSuccess: true });
@@ -90,23 +82,21 @@ const collectionData = await tokenClient.getCollectionData(
   alice.address(),
   collectionName
 );
-console.log(`Alice's collection: ${JSON.stringify(collectionData, null, 4)}`); // <:!:section_6
+console.log(`Alice's collection: ${JSON.stringify(collectionData, null, 4)}`);
 
 // Get the token balance.
-// :!:>section_7
 const aliceBalance1 = await tokenClient.getToken(
   alice.address(),
   collectionName,
   tokenName,
   `${tokenPropertyVersion}`
 );
-console.log(`Alice's token balance: ${aliceBalance1["amount"]}`); // <:!:section_7
+console.log(`Alice's token balance: ${aliceBalance1["amount"]}`);
 
 // Get the token data.
-// :!:>section_8
 const tokenData = await tokenClient.getTokenData(
   alice.address(),
   collectionName,
   tokenName
 );
-console.log(`Alice's token data: ${JSON.stringify(tokenData, null, 4)}`); // <:!:section_8
+console.log(`Alice's token data: ${JSON.stringify(tokenData, null, 4)}`);
